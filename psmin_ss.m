3e8817a9061c563a6e5130759ba288afd285651d
yjzhang3 <yjzhang3@bu.edu>
2021-12-06 17:34:15 -0600

    explored higher TF range

--- psmin_ss.m
+++ psmin_ss.m
@@ -1,13 +1,23 @@
-function [lg,x] = psmin_ss(TF)
+function [lg_max,TF_max,x] = psmin_ss(TF)
 rng default
-% lb = [1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5];
-lb = [1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5 1E-5];
-% ub = [20,20,20,20,20,20,20,20,20];
-ub = [20,20,20,20,20,20,20,20];
+lb = zeros(1,8)+1E-2; % neq
+% lb = zeros(1,7)+1E-5; % eq
+ub = zeros(1,8)+1E+4; % neq
+% ub =  zeros(1,7)+1E+4; % eq
 fun = @(p) fit_model_ss(p,TF);
-% nvars = 9;
-nvars = 8;
-x = particleswarm(fun,nvars,lb,ub)
-%% particle swarm results
-lg = lg_TF_ss(x,TF)
+nvars = 8; % neq
+% nvars = 7; % eq
+
+%% more iterations, not just once
+lg_max = 0;
+TF_max = 0;
+for iteration = 1:300
+    x = particleswarm(fun,nvars,lb,ub)
+    [lg,TF] = lg_TF_ss(x,TF);
+    if lg>lg_max
+        lg_max = lg;
+        TF_max = TF;
+    end
+end
+
 end
