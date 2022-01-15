a8b2754f9aa1a67c1684920640da114d247239b1
yjzhang3 <yjzhang3@bu.edu>
2022-01-15 15:12:45 -0600

    clean up branch

--- validity.m
+++
@@ -1,6 +0,0 @@
-%% test if the TF that maximizes the log gain is always the largest log gain
-% p1 = rand([8 1])';
-p2 = rand([8 1]).^0.45+abs(log(p1)).*100';
-
-[maxLG1,maxTF1,log_gain1] = lg_TF_ss(p2(1,:),100);
-% [maxLG2,maxTF2,log_gain2] = lg_TF_ss(p2,20);
