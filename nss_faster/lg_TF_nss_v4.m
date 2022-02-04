33dd9e1d4778249f549ad88bcc0d75f0d5bbbcb3
Yujia <56132313+yjzhang3@users.noreply.github.com>
2022-02-04 12:43:43 -0600

    include t = 0 simulation and see non-monotonic

--- nss_faster/lg_TF_nss_v4.m
+++ nss_faster/lg_TF_nss_v4.m
@@ -5,7 +5,7 @@ function [maxLG,maxT,LG_overall] = lg_TF_nss_v4(p,t)
 % different from v2, TF is packed into the vector of parameters
 % output: the log gain in TR w.r.t this TF over time
 
-tspan = [0.1:0.1:t];
+tspan = [0:0.001:t];
 
 for tt = 1:length(tspan)
     [LG,y] = mat_exp_sol(p,tspan(tt));
